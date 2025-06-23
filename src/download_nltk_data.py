78223e412a4b4c3a733fa14f033549da684740dc
Semih Çakır <semihcakir04@gmail.com>
2025-06-23 23:11:53 +0300

    Neccessities

---
+++ src/download_nltk_data.py
@@ -0,0 +1,9 @@
+import nltk
+
+print("Downloading 'stopwords' package...")
+nltk.download('stopwords')
+
+print("\nDownloading 'wordnet' package...")
+nltk.download('wordnet')
+
+print("\nDownloads complete!")
